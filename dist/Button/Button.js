3d704e26b62807cf501001628b7ec16301bec38b
mnik01 <maksimgrinberg01@gmail.com>
2021-09-28 00:42:32 +0600

    exp

---
+++ dist/Button/Button.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import 'react';
+export const Button = () => (_jsx("button", { children: "I SHOULD BE HERE" }, void 0));
+//# sourceMappingURL=Button.js.map
---
+++ dist/Link/Link.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import 'react';
+export const Link = () => (_jsx("a", Object.assign({ href: '#' }, { children: "I DONT NEED TO BE HERE" }), void 0));
+//# sourceMappingURL=Link.js.map
