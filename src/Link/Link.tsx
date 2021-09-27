10abd3ab2f229b7b389856c53537f85bf7fed1d5
mnik01 <maksimgrinberg01@gmail.com>
2021-09-27 22:32:11 +0600

    feat: treeshaking added

---
+++ src/Link/Link.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react'
+
+export const Link: FC = () => (<a href='#'>I DONT NEED TO BE HERE</a>);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+import path from "path";
+
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "mui01",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled into your library
+      external: ["react"],
+    },
+  },
+});
